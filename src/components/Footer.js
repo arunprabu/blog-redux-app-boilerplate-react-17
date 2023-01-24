48dc56505600da7700e5770cada4048af48d8746
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-01-24 16:56:18 +0530

    Revert "updated"
    This reverts commit 04f39efa163c9f08cd8f4f74641a8bc5133662f5.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-center'>
-      <hr />
-      <p>Copyright 2022</p>
-    </div>
-  );
+    <div>Copyright 2022</div>
+  )
 }
 
 export default Footer
--- src/index.js
+++ src/index.js
@@ -3,20 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import configureStore from './configureStore';
-import { Provider } from 'react-redux';
-
-// Let's get the data from store
-const store = configureStore();
-console.log(store);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}> { /* providing store data to the whole app */ }
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
